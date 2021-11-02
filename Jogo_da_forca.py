c81d64f4f9a9a5150afd69af514c0aab3d0bd121
Luiz Maurício <2003lmvp@gmail.com>
2021-11-02 13:40:03 -0300

    Adicionei a função de percorrimento da palavra e checagem se a letra está ou não correta.

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -23,9 +23,38 @@ def tentativa_player(): #Percorre-se a string da forca e define se o player acer
     tentativa = str(input()) #Se o jogador acertar, revela-se parte da palavra. Uma vez estando a palavra toda revelada, o jofador vence (vitoria == True) e fecha-se o game.
     pass #Uma pessoa pode fazer o percorrimento e a outra atrás do design do bonequinho da forca, da barra das palavras etc.
 
+def percorrimento_palavra(guess, palavra):
+    #esta função visa detectar se a letra do input esta na palavra selecionada ou não
+    #função que faz a checagem pela letra na palavra
+
+    global vidas #esta variável conta a quantidade de erros
+    global posicoes_a_revelar #lista com a posição dos caracteres da palavra secreta a serem revelados
+    
+    if guess in palavra:
+        #caso a letra esteja na palavra secreta
+
+        print("acertou")
+        
+        for x in range(len(palavra)):
+            #este loop percorre a palavra secreta e registra todas as posições na qual a letra adivinhada é encontrada, mesmo se houver repetições
+            
+            if guess == palavra[x] and x not in posicoes_a_revelar:
+                #esta condicional impede que a mesma posição seja registrada várias vezes no vetor posicoes_a_revelar
+                posicoes_a_revelar.append(x)
+    
+    else:
+        #caso a letra não esteja na palavra secreta
+        
+        print("errou")
+        
+        vidas -= 1 #Diminui 1 do total de vidas
+    pass
+
 vidas = 5 #Número de tentativas. Podemos mudá-la pensando na dificuldade.
 vitoria = False #Variável para vencer o jogo.
 play = True #Variável para fechar o jogo, ou dar play again.
+posicoes_a_revelar = [] #este vetor guarda todas as posições dos caracteres corretos
+
 while play: #Main loop do game. Eu (Rodrigo) posso fazer se preferirem.
     while vidas > 0:
         chave_aleatoria
