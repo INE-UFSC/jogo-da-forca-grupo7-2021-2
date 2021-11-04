837461c23726461a669c02a4319be5cc837952aa
Alien_Love <86534496+Alien-Love@users.noreply.github.com>
2021-11-04 08:36:37 -0300

    adiciona função do boneco

---
+++ hangman.py
@@ -0,0 +1,127 @@
+global vidas
+
+def draw(vidas):
+
+
+
+    if vidas == 6:
+               
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |       |      ')
+        print('  |              ')
+        print('  |              ')
+        print('  |              ') 
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')  
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('__|_____________ ')
+    
+
+    if vidas == 5:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / o o \   ')
+        print('  |    \__~__/   ')
+        print('  |       |      ') 
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')  
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('__|_____________ ')
+    
+    if vidas == 4:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / o o \   ')
+        print('  |    \__~__/   ')
+        print('  |       |      ') 
+        print('  |       |      ')
+        print('  |       |      ')
+        print('  |       |      ')
+        print('  |              ')  
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('__|_____________ ')
+        
+    if vidas == 3:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / o o \   ')
+        print('  |    \__~__/   ')
+        print('  |       |      ') 
+        print('  |      /|      ')
+        print('  |     / |      ')
+        print('  |    @  |      ')
+        print('  |              ')  
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('__|_____________ ')
+    
+    if vidas == 2:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / o o \   ')
+        print('  |    \__~__/   ')
+        print('  |       |      ') 
+        print('  |      /|\     ')
+        print('  |     / | \    ')
+        print('  |    @  |  @   ')
+        print('  |              ')  
+        print('  |              ')
+        print('  |              ')
+        print('  |              ')
+        print('__|_____________ ')    
+    
+    if vidas == 1:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / o o \   ')
+        print('  |    \__0__/   ')
+        print('  |       |      ') 
+        print('  |      /|\     ')
+        print('  |     / | \    ')
+        print('  |    @  |  @   ')
+        print('  |      /       ')  
+        print('  |     /        ')
+        print('  |    =         ')
+        print('  |              ')
+        print('__|_____________ ')
+
+    if vidas == 0:
+       
+        print('  _______        ')    
+        print('  |      \       ')
+        print('  |     __|__    ')
+        print('  |    / x x \   ')
+        print('  |    \__0__/   ')
+        print('  |       |      ') 
+        print('  |      /|\     ')
+        print('  |     / | \    ')
+        print('  |    @  |  @   ')
+        print('  |      / \     ')  
+        print('  |     /   \    ')
+        print('  |    =     =   ')
+        print('  |              ')
+        print('__|_____________ ')
+
+    
